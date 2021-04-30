defa217c2506d5795780fb1885848e19a2d4ffe7
赵锐飞 <peterchezhao@gmail.com>
2021-04-30 17:50:50 +0800

    原始duckling代码提交

---
+++ Duckling/Rules/JA.hs
@@ -0,0 +1,48 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree.
+
+
+{-# LANGUAGE GADTs #-}
+
+
+module Duckling.Rules.JA
+  ( defaultRules
+  , langRules
+  , localeRules
+  ) where
+
+import Duckling.Dimensions.Types
+import Duckling.Locale
+import Duckling.Types
+import qualified Duckling.Numeral.JA.Rules as Numeral
+import qualified Duckling.Ordinal.JA.Rules as Ordinal
+import qualified Duckling.Temperature.JA.Rules as Temperature
+import qualified Duckling.TimeGrain.JA.Rules as TimeGrain
+
+defaultRules :: Seal Dimension -> [Rule]
+defaultRules = langRules
+
+localeRules :: Region -> Seal Dimension -> [Rule]
+localeRules region (Seal (CustomDimension dim)) = dimLocaleRules region dim
+localeRules _ _ = []
+
+langRules :: Seal Dimension -> [Rule]
+langRules (Seal AmountOfMoney) = []
+langRules (Seal CreditCardNumber) = []
+langRules (Seal Distance) = []
+langRules (Seal Duration) = []
+langRules (Seal Email) = []
+langRules (Seal Numeral) = Numeral.rules
+langRules (Seal Ordinal) = Ordinal.rules
+langRules (Seal PhoneNumber) = []
+langRules (Seal Quantity) = []
+langRules (Seal RegexMatch) = []
+langRules (Seal Temperature) = Temperature.rules
+langRules (Seal Time) = []
+langRules (Seal TimeGrain) = TimeGrain.rules
+langRules (Seal Url) = []
+langRules (Seal Volume) = []
+langRules (Seal (CustomDimension dim)) = dimLangRules JA dim
